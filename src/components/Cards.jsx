59c22e267a11a8cfe12c188031db971ef3e08fd6
Shanu30 <ksechenari@gmail.com>
2022-05-14 22:40:02 +0530

    update responsiveness

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,10 +6,10 @@ export default function Cards() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
   const getData = async () => {
-    let res = await fetch("http://localhost:3000/data");
+    let res = await fetch(`https://gateway.marvel.com/v1/public/characters?limit=30&apikey=530be097180a0b87c441fd298d5c9350&hash=787872091c09ef8af63feecb77d30fc1&ts=1`);
     let list = await res.json();
-    setData(list);
-  };
+    setData(list.data.results);
+  };    
   useEffect(() => {
     getData();
   }, []);
@@ -35,13 +35,19 @@ export default function Cards() {
             }
         })
         .map((item,index) => {
+            let path = item.thumbnail.path;
+            let extension = item.thumbnail.extension;
+            let imgLink = `${path}.${extension}`;
+            // console.log(imgLink)
           return (
             <div className={styles.card} key={index}>
+                
               <div className={styles.img}>
-                <img src={item.thumbnail} alt={item.name} />
+
+                <img src={imgLink} alt={item.name} />
               </div>
               <div className={styles.info}>
-                <p className={styles.name}>{item.name}</p>
+                <p className={styles.name}>{item.name.toUpperCase()}</p>
               </div>
             </div>
           );
